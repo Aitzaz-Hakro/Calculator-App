a16fcd01a96265ae7b08909dc5b03fb95260ae2c
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-03-17 10:21:47 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+ document.getElementById('btn')
+
+function appendNumber(num) {
+    var div = document.createElement('div');
+    div.innerHTML = num;
+    document.body.appendChild(div);
+  
+
+}
+
+
+
+// var div = document.createElement('div');
+//   div.innerHTML = 1;
+//   document.body.appendChild(div);
